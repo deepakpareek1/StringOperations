b907eccdb7a61150b44f1a543d603275ec1e2bfc
NSF\dpareek <dpareek@nsf.org>
2022-05-29 20:18:30 +0530

    Set input format according multiple options Set operation functions

--- src/com/string/operations/MainClass.java
+++ src/com/string/operations/MainClass.java
@@ -1,5 +1,8 @@
 package com.string.operations;
 
+import java.io.File;
+import java.util.ArrayList;
+
 public class MainClass {
 
 	public MainClass() {
@@ -10,10 +13,15 @@ public class MainClass {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		   
-		String input = Operations.readInput();
 		
-		String result = Operations.performOperation(input);
+		File file = Operations.getFile("C:/Users/dpareek/Documents/dictionary.txt");
+		
+		ArrayList<String> input = Operations.readInput();
+		
+		String option = input.get(0);
+		String str = input.get(1);
+		
+		String result = Operations.performOperation(file, option, str);
 		
 		Operations.printOutput(result);
 		
--- src/com/string/operations/Operations.java
+++ src/com/string/operations/Operations.java
@@ -2,6 +2,8 @@ package com.string.operations;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Operations {
@@ -31,34 +33,61 @@ public class Operations {
 	 * Reading from standard input
 	 * @param: 
 	 */
-	public static String readInput() {
+	public static ArrayList<String> readInput() {
+		ArrayList<String> input = new ArrayList<String>();
+		String option = "";
 		String str = "";
 		Scanner sc= new Scanner(System.in); //System.in is a standard input stream
 		try {
-			System.out.println("Input: ");
-			str= sc.nextLine(); //reads string
+
+			System.out.println("1: Check if a word exists");
+			System.out.println("2: Correct case");
+			System.out.println("3: Correct punctuation");
+			System.out.println("4: Correct duplicate letters");
+			System.out.println("5: Correct adjacent letters");
+			System.out.println("6: Transposing letters");
+			System.out.println("Please enter operation number:");
+			option = sc.nextLine(); //reads option
+			System.out.println("Please enter operation string:");
+			str = sc.nextLine(); // reads string
 			sc.close();
+			input.add(option);
+			input.add(str);
 		} catch (Exception ex) {
 			System.out.println("Error in reading input: " + ex.getMessage());
 		} finally {
 			sc.close();
 		}
-		return str;
+		return input;
 	}
 	
 	/*
 	 * 
 	 * @param: 
 	 */
-	public static String performOperation(String input) {
-		File file = getFile("D:\\dictionary.txt");
-		return "0";
+	public static String performOperation(File file, String option, String input) {
+		String output = "Incorrect Input";
+		
+		if (option.equals("1")) {
+			output = findWord(file, input);
+		} else if (option.equals("2")) {
+			
+		} else if (option.equals("3")) {
+			
+		} else if (option.equals("4")) {
+			
+		} else if (option.equals("5")) {
+			
+		} else if (option.equals("6")) {
+			
+		}
+		return output;
 		
 	}
 	
 	/*
-	 * Printing result
-	 * @param: result
+	 * Exercise 1: Check if a word exists
+	 * @param: file, matchWord
 	 */
 	public static String findWord(File file, String matchWord) {
 		String word = null;
@@ -77,6 +106,46 @@ public class Operations {
 			System.out.println("Read file error: "+ e.getMessage());
 		}
 		return word;
+	}
+	
+	/*
+	 * Exercise 2: Correct case
+	 * @param: file, matchWord
+	 */
+	public static String getCorrectCase(File file, String matchWord) {
+		return "";
+	}
+	
+	/*
+	 * Exercise 3: Correct punctuation
+	 * @param: file, matchWord
+	 */
+	public static String getCorrectPunctuation(File file, String matchWord) {
+		return "";
+	}
+	
+	/*
+	 * Exercise 4: Correct duplicate letters
+	 * @param: file, matchWord
+	 */
+	public static String getCorrectDuplicateLetters(File file, String matchWord) {
+		return "";
+	}
+	
+	/*
+	 * Exercise 5: Correct adjacent letters
+	 * @param: file, matchWord
+	 */
+	public static String getCorrectAdjacentLetters(File file, String matchWord) {
+		return "";
+	}
+	
+	/*
+	 * Exercise 6: Transposing letters
+	 * @param: file, matchWord
+	 */
+	public static String getTransposingLetters(File file, String matchWord) {
+		return "";
 	}
 	
 	/*
